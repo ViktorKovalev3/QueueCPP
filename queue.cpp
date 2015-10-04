dec2fbb276e0c7bbff859ca30583be575ad9b16f
Viktor <ya-72480@yandex.ru>
2015-10-04 18:07:37 +0300

    GitNoob

--- queue.cpp
+++ queue.cpp
@@ -2,10 +2,6 @@
 
 Queue::Queue(int initiatingItem)
 {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 9bb61b04d1f5f95c26d457577bd175e2791fde3c
     first = new (QueueItem);
     last  = first;           //Because one item
     first->value = initiatingItem;
@@ -13,11 +9,3 @@ Queue::Queue(int initiatingItem)
 
 void Queue::enqueue(){
 }
-<<<<<<< HEAD
-=======
-=======
-
-}
-
->>>>>>> f4e49ad7a3c78e8e564ebfaa443a54cd6af49d55
->>>>>>> 9bb61b04d1f5f95c26d457577bd175e2791fde3c
