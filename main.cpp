8833c6ef522d2b2b82dc62d9f45c31d1ad93b645
Viktor Kovalev <ya72480@gmail.com>
2015-10-04 21:51:53 +0300

    Сделал шаблон для универсальности структуры

--- main.cpp
+++ main.cpp
@@ -5,13 +5,22 @@ using namespace std;
 
 int main()
 {
-    int a = 1;
-    Queue myQueue(2);
-    myQueue.enqueue(a);
-    myQueue.enqueue(1024);
-    cout << myQueue.dequeue() << endl;
-    cout << myQueue.dequeue() << endl;
-    cout << myQueue.dequeue() << endl;
-    cout << myQueue.dequeue() << endl; //Error, because queue is empty
+    int a = 2;
+    Queue<int> myIntQueue(1);
+    myIntQueue.enqueue(a);
+    myIntQueue.enqueue(3);
+
+    cout << myIntQueue.dequeue() << endl;
+    cout << myIntQueue.dequeue() << endl;
+    cout << myIntQueue.dequeue() << endl;
+    //cout << myIntQueue.dequeue() << endl; //Error, because queue is empty
+
+    Queue<string> myStrQueue("one");
+    myStrQueue.enqueue      ("two");
+    myStrQueue.enqueue      ("three");
+
+    for (size_t i = 0; i < myStrQueue.getSize(); i++){
+        cout << myStrQueue.dequeue() << endl;
+    }
     return 0;
 }
--- queue.h
+++ queue.h
@@ -1,21 +1,77 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include <iostream>  //cerr stream
+#include <stdlib.h>  //abort()
+template <typename T>
 
+/*---------------------------------------------------------*/
 class Queue
 {
 private:
     struct QueueItem{
-        int value;
+        T value;
         QueueItem *nextAdr;
     };
     QueueItem *first, *last;
-    int size;
+    size_t size;
 
 public:
-    Queue(int initiatingItem);
-    void enqueue(int value);  //Add new last item to end
-    int dequeue (  void   );  //Request first item and del
-    int getSize (  void   );
+    Queue(const T &);
+    ~Queue();
+    void enqueue(T value);  //Add new last item to end
+    T dequeue (  void   );  //Request first item and del
+    size_t getSize (  void   );
 };
+
+/*---------------------------------------------------------*/
+template <typename T>
+Queue<T>::Queue(const T & initiatingItem)
+{
+    first = new (QueueItem);
+    first->nextAdr = 0;
+    last  = first;           //Because one item
+    first->value = initiatingItem;
+    size = 1;
+}
+
+template<typename T>
+Queue<T>::~Queue()
+{
+    first = 0;
+    last = 0;
+    delete first;
+    delete last;
+}
+
+template <typename T>
+void Queue<T>::enqueue(T value){
+    QueueItem *newTmpLast = new(QueueItem);
+    newTmpLast->nextAdr = 0;
+    newTmpLast->value   = value;
+
+    last->nextAdr = newTmpLast;
+    last = newTmpLast;
+    size++;
+}
+
+template <typename T>
+T Queue<T>::dequeue(void){
+    if(size == 0){
+        std::cerr << "!!\t dequeue Empty Queue \t!! \n";
+        abort();
+    }
+    T tmpVal = first->value;
+    QueueItem *tmpPointerToFirst = first; // Need for delete old First Queue Item
+    first = first->nextAdr;               // Change new adress for First
+    delete tmpPointerToFirst;             // Delete old First Item
+    size--;
+
+    return tmpVal;
+}
+
+template <typename T>
+size_t Queue<T>::getSize(void){
+    return size;
+}
 
 #endif // QUEUE_H
