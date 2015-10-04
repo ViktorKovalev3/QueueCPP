d302abe494c5f4f1748da6af2564c5b3e9e970e8
Viktor <ya-72480@yandex.ru>
2015-10-04 18:07:37 +0300

    GitNoob

--- queue.cpp
+++ queue.cpp
@@ -2,22 +2,11 @@
 
 Queue::Queue(int initiatingItem)
 {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 9bb61b04d1f5f95c26d457577bd175e2791fde3c
     first = new (QueueItem);
+    first->prevAdr = 0;
     last  = first;           //Because one item
     first->value = initiatingItem;
 }
 
 void Queue::enqueue(){
 }
-<<<<<<< HEAD
-=======
-=======
-
-}
-
->>>>>>> f4e49ad7a3c78e8e564ebfaa443a54cd6af49d55
->>>>>>> 9bb61b04d1f5f95c26d457577bd175e2791fde3c
